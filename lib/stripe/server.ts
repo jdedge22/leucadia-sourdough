805d3964c73901436104c12bfc4a6b0000bd4f11
Jim Edgett <jimedgett@Jims-MacBook-Air.local>
2026-02-19 15:54:29 -0600

    Add missing Stripe price ID exports

--- lib/stripe/server.ts
+++ lib/stripe/server.ts
@@ -4,3 +4,6 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2026-01-28.clover',
   typescript: true,
 })
+
+export const STRIPE_PRICE_ID_BREAD_ONLY = process.env.STRIPE_PRICE_ID_BREAD_ONLY!
+export const STRIPE_PRICE_ID_BREAD_PASTRIES = process.env.STRIPE_PRICE_ID_BREAD_PASTRIES!
