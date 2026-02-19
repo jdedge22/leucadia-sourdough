d4a6655d10043f648c8311f35ebc5d0740c1bef2
Jim Edgett <jimedgett@Jims-MacBook-Air.local>
2026-02-19 15:52:05 -0600

    Fix Stripe API version in lib/stripe/server.ts

--- lib/stripe/server.ts
+++ lib/stripe/server.ts
@@ -1,8 +1,6 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2026-01-28.clover',
   typescript: true,
 })
-
-export const STRIPE_PRICE_ID_BREAD_ONLY = process.env.STRIPE_PRICE_ID_BREAD_ONLY!
