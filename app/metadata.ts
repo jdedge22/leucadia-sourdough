27db27910ff545b138f62f093e779d4cc1cde436
Jim Edgett <jimedgett@Jims-MacBook-Air.local>
2026-02-19 16:13:29 -0600

    Add mobile hamburger menu to navigation

---
+++ app/metadata.ts
@@ -0,0 +1,4 @@
+export const metadata = {
+  title: 'Leucadia Sourdough | Fresh-Milled Organic Sourdough Delivery',
+  description: '40% more nutrients than store-bought bread. Fresh-milled organic sourdough delivered weekly to North County San Diego.',
+}
